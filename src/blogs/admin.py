b0c4b9c82b9e590266fdcedda12d82cce28cfa8b
DESKTOP-VSIK1O3\VIRIATO <javi.lindo@gmail.com>
2018-01-02 19:17:53 +0100

    limpieza de imagenes

--- src/blogs/admin.py
+++ src/blogs/admin.py
@@ -5,4 +5,28 @@ admin.site.site_header = 'WordPlease Backoffice'
 admin.site.site_title = 'WordPlease Backoffice'
 
 admin.site.register(Category)
-admin.site.register(Post)
+
+
+@admin.register(Post)
+class MovieAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'image', 'intro', 'publish_date')
+    list_filter = ('categories',)
+    search_fields = ('title',)
+    readonly_fields = ('created_at', 'modified_at')
+    fieldsets = (
+        ('General Data', {
+            'fields': ('title', 'intro', 'body')
+        }),
+        ('Categories and Publish Date', {
+            'fields': ('categories', 'publish_date')
+        }),
+        ('Additional Info', {
+            'fields': ('user', 'image'),
+        }),
+        ('Created & Modified', {
+            'fields': ('created_at', 'modified_at'),
+            'classes': ('collapse',),
+            'description': 'These fields are auto-generated'
+        }),
+    )
