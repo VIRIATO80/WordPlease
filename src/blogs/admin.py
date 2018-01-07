91d08c0eaf501a9ab8f916c119bffb5720ac6cf1
DESKTOP-VSIK1O3\VIRIATO <javi.lindo@gmail.com>
2018-01-07 16:56:05 +0100

    Añadimos modelo Blog al administrador. Se había olvidado

--- src/blogs/admin.py
+++ src/blogs/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from blogs.models import Category, Post
+from blogs.models import Category, Post, Blog
 
 admin.site.site_header = 'WordPlease Backoffice'
 admin.site.site_title = 'WordPlease Backoffice'
 
-admin.site.register(Category)
+admin.site.register(Category),
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+
+    list_display = ('blog_name', 'blog_description')
 
 
 @admin.register(Post)
-class BlogAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'image', 'intro', 'publish_date')
     list_filter = ('categories',)
